99a917102dbb25c670c462a11eba9c23d7c54bb2
Ian <ian.explore.world@gmail.com>
2019-07-21 18:23:00 +0900

    Added longest palindrome

---
+++ algo/_005_LongestPalindromicSubstring.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        result = ""
+        for center in range(len(s)):
+            sub = self.findLongestPalindrome(s, center, center)
+            if len(sub) > len(result):
+                result = sub 
+            sub = self.findLongestPalindrome(s, center, center+1)
+            if len(sub) > len(result):
+                result = sub 
+        return result
+    
+    def findLongestPalindrome(self, s, left, right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left  -= 1
+            right += 1
+        return s[left+1:right]
+        
