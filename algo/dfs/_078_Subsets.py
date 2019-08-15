f2aee649484d81fe407f750e6be8af1250af923e
Ian <ian.explore.world@gmail.com>
2019-08-15 22:04:02 +0900

    Double method for subsets

--- algo/dfs/_078_Subsets.py
+++ algo/dfs/_078_Subsets.py
@@ -1,5 +1,8 @@
+from typing import List
+import time
+
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsetsDFS(self, nums: List[int]) -> List[List[int]]:
         if not nums:
             return []
         ans = []
@@ -12,4 +15,37 @@ class Solution:
         self.dfs(nums, ans, curArr+[nums[index]], index+1)
         self.dfs(nums, ans, curArr, index+1)
         return
-        
+    
+    def subsetsDouble(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        ansArr = []
+        ansArr.append([])
+        ansArr.append([nums[0]])
+        #print(ansArr)
+        for i in range(1, len(nums)):
+            #newArr = ansArr[:]  #new list (but only 1 layer)
+            newArr = [cur[:] for cur in ansArr] #new list
+            #print("newArr:", newArr)
+            for cur in newArr:
+                cur.append(nums[i])
+            ansArr.extend(newArr)
+            #print("ansArr:", ansArr)
+        return ansArr
+
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+s = Solution()
+#print("DFS   :", s.subsetsDFS(nums))
+#print("Double:", s.subsetsDouble(nums))
+
+# Time measurment 
+t1_s = time.process_time_ns()
+s.subsetsDFS(nums)
+t1_e = time.process_time_ns()
+
+t2_s = time.process_time_ns()
+s.subsetsDouble(nums)
+t2_e = time.process_time_ns()
+
+print("Time elapsed - DFS    (ms)", (t1_e - t1_s)/1000000) # CPU seconds elapsed (floating point)
+print("Time elapsed - Double (ms)", (t2_e - t2_s)/1000000) # CPU seconds elapsed (floating point)
