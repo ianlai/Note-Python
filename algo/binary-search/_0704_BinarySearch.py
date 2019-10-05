83241c8c9e00abf0b69ec608118241db88fdd845
Ian <ian.explore.world@gmail.com>
2019-10-05 18:13:48 +0900

    Extended BinarySearch to searchAny, searchFirst, searchLast

--- algo/binary-search/_0704_BinarySearch.py
+++ algo/binary-search/_0704_BinarySearch.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def searchAny(self, nums: List[int], target: int) -> int:
         if not nums: 
             return -1
         start, end = 0, len(nums)-1
@@ -18,7 +20,57 @@ class Solution:
             return start
         if target == nums[end]:
             return end
- 
         return -1
+
+    def searchFirst(self, nums: List[int], target: int) -> int:
+        if not nums or len(nums)==0: 
+            return -1
+        start, end = 0, len(nums)-1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if target == nums[mid]:
+                end = mid 
+            elif target < nums[mid]:
+                end = mid
+            else: 
+                start = mid
+        
+        if target == nums[start]:
+            return start
+        if target == nums[end]:
+            return end
+        return -1
+    
+    def searchLast(self, nums: List[int], target: int) -> int:
+        if not nums or len(nums)==0: 
+            return -1
+        start, end = 0, len(nums)-1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if target == nums[mid]:
+                start = mid 
+            elif target < nums[mid]:
+                end = mid
+            else: 
+                start = mid
+        
+        if target == nums[start]:
+            return start
+        if target == nums[end]:
+            return end
+        return -1
+    
+s = Solution()
+nums = [1,2,2,3,3,3,3,3,9] 
+target = 3 
+print("Find", target, "in", nums, "(any)  : ", s.searchAny(nums, target))
+print("Find", target, "in", nums, "(first): ", s.searchFirst(nums, target))
+print("Find", target, "in", nums, "(last) : ", s.searchLast(nums, target))
+
+target = 4
+print("Find", target, "in", nums, "(any)  : ", s.searchAny(nums, target))
+print("Find", target, "in", nums, "(first): ", s.searchFirst(nums, target))
+print("Find", target, "in", nums, "(last) : ", s.searchLast(nums, target))
+
                 
             
