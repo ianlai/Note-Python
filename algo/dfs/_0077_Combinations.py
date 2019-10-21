438b0512339fea26a4aba882b6209ac7da94ed71
Ian <ian.explore.world@gmail.com>
2019-10-22 01:44:45 +0900

    Modified _0077_Combinations.py

--- algo/dfs/_0077_Combinations.py
+++ algo/dfs/_0077_Combinations.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution:
-    
-    ### OK
+    #===================================
+    ### Best (use different array in each level)
     def combine(self, n: int, k: int) -> List[List[int]]:
         if n == 0 or k == 0:
             return []
@@ -14,30 +14,44 @@ class Solution:
             ans.append(cur)
             return 
         for i in range(nidx, n + 1):
-            
-            # Wrong
-            # cur.append(i)
-            # self.bfs(n, k, ans, cur , i + 1)
-            # cur = cur[:len(cur)-1] 
-
-            # Correct
             self.bfs(n, k, ans, cur + [i], i + 1)
         return 
     
-      ### OK, but kidx not necessary 
+    #===================================
+    ### OK (use cur one array to do the traverse, only clone to new in the end list())
 #     def combine(self, n: int, k: int) -> List[List[int]]:
 #         if n == 0 or k == 0:
 #             return []
 #         ans = []
-#         self.bfs(n, k, ans, [], 1, 0)
+#         self.bfs(n, k, ans, [], 1)
 #         return ans
     
-#     def bfs(self, n, k, ans, cur, nidx, kidx):
+#     def bfs(self, n, k, ans, cur, nidx):
+#         if len(cur) == k:
+#             #ans.append(list(cur))  #create new array
+#             ans.append(cur[:])      #create new array
+#             return 
+#         for i in range(nidx, n + 1):
+#             cur.append(i)
+#             self.bfs(n, k, ans, cur, i + 1)
+#             cur.remove(i)
+#         return 
+    
+    #===================================
+    ## Fine, but not preferred, actually kidx not necessary 
+#     def combine2(self, n: int, k: int) -> List[List[int]]:
+#         if n == 0 or k == 0:
+#             return []
+#         ans = []
+#         self.bfs2(n, k, ans, [], 1, 0)
+#         return ans
+    
+#     def bfs2(self, n, k, ans, cur, nidx, kidx):
 #         if kidx == k:
 #             ans.append(cur)
 #             return 
 #         for i in range(nidx, n + 1):
-#             self.bfs(n, k, ans, cur + [i], i + 1, kidx + 1)
+#             self.bfs2(n, k, ans, cur + [i], i + 1, kidx + 1)
 #         return 
 
 s = Solution()
