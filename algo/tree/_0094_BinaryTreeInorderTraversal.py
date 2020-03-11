d218a0fe4f48eb3c87c109a8f0bb188c703d61cb
Ian <ian.explore.world@gmail.com>
2020-03-12 00:03:31 +0900

    Updated _0094_BinaryTreeInorderTraversal.py

--- algo/tree/_0094_BinaryTreeInorderTraversal.py
+++ algo/tree/_0094_BinaryTreeInorderTraversal.py
@@ -1,14 +1,36 @@
-from typing import List
-
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
+    
+    #Jiuzhang (hard to understand)
     def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        res = []    #array
+        stack = []  #stack 
+        
+        dummy = TreeNode(0)
+        dummy.right = root
+        stack.append(dummy)
+        
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.right: 
+                cur = cur.right 
+                while cur:
+                    stack.append(cur)
+                    cur = cur.left
+            if not stack:
+                break
+        return res[1:]  #remove dummy 
+    
+    def inorderTraversal2(self, root: TreeNode) -> List[int]:
         if root is None:
             return []
         res = []    #array
@@ -19,17 +41,18 @@ class Solution:
         while stack:
             if cur.left:
                 stack.append(cur.left)
-                cur = cur.left
+                cur = cur.left         #the left bottom node will be in the right (outmost) of stack (LEFT)
             else:
                 out = stack.pop()  
-                res.append(out.val)    #use the node
-                if out.right:
+                res.append(out.val)    #use the node (CENTER)
+                if out.right:          #(RIGHT)
                     cur = out.right
                     stack.append(cur)
         return res
             
     #================================================
     
+
     def inorderTraversal_recursive(self, root: TreeNode) -> List[int]:
         if root is None:
             return []
