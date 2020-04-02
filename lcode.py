2430657915e6fc436339c8ad3d30bf4e1ed96829
Ian <ian.explore.world@gmail.com>
2020-04-03 01:26:27 +0900

    Added feature to calculate solved/total number of questions in each difficulty level

--- lcode.py
+++ lcode.py
@@ -26,11 +26,22 @@ if __name__ == '__main__':
         r = s.get(API_URL)
         #print("### header:", "\n", r.headers)
         my_result = json.loads(r.text)
-    print('user_name:' , my_result['user_name'])
-    print('num_total:' , my_result['num_total'])
+    print('User Name:' , my_result['user_name'])
     my_statistic_data = {key: my_result[key] for key in ['ac_easy', 'ac_hard', 'ac_medium', 'num_solved']}
-    print(my_statistic_data)
+
+    count_easy   = 0
+    count_medium = 0
+    count_hard   = 0
+    for q in my_result['stat_status_pairs']:
+        if q['difficulty']['level'] == 1:
+            count_easy += 1
+        if q['difficulty']['level'] == 2:
+            count_medium += 1
+        if q['difficulty']['level'] == 3:
+            count_hard += 1
+    print('Solved / Total (Easy)  :' , my_result['ac_easy']   , '/', count_easy)
+    print('Solved / Total (Medium):' , my_result['ac_medium'] , '/', count_medium)
+    print('Solved / Total (Hard)  :' , my_result['ac_hard']   , '/', count_hard)
+    print('Solved / Total (All)   :' , my_result['num_solved'], '/', my_result['num_total'])
     
-    # for q in my_result['stat_status_pairs']:
-    #     if q['difficulty']['level'] == 2:
-    #         print(q['stat']['question_id'], q['stat']['question__title'])
+            #print(q['stat']['question_id'], q['stat']['question__title'])
