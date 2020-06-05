d5131f33452cc4e2ff85f148c47026af2728306c
Ian <ian.explore.world@gmail.com>
2020-06-05 22:41:54 +0900

    Reviewed _0141_LinkedListCycle.py, _0142_LinkedListCycle2.py, _0160_IntersectionOfTwoLinkedLists.py

--- review/_0206_ReverseLinkedList.py
+++ review/_0206_ReverseLinkedList.py
@@ -1,32 +1,26 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x=None):
-        self.val = x
-        self.next = None
-
-class List(object):
-    def __init__(self, root=None):
-        self.root = root
-    def init_from_list(self, mlist):
-        if mlist:
-            self.root = ListNode(mlist[0])
-            cur = self.root
-            for i in range(1, len(mlist)):
-                node = ListNode(mlist[i])
-                cur.next = node
-                cur = node
-    def traverse(self):
-        cur = self.root
-        while cur:
-            print(str(cur.val) + " -> ", end = '') 
-            cur = cur.next
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-    def reverseList(self, head):  # Iterative
+
+    def reverseListSimple(self, head):  # Iterative
         prev, curr = None, head
         while curr:
             curr.next, prev, curr = prev, curr, curr.next
         return prev
+    #==================================================
+    def reverseList(self, head):
+        print("reverseList0")
+        if not head or not head.next: 
+            return head
+        
+        p1, p2 = None, head
+        while p2:
+            p2.next, p1, p2 = p1, p2, p2.next
+        return p1
      #==================================================
     def reverseList1(self, head: ListNode) -> ListNode:
         print("reverseList1")
@@ -61,17 +55,3 @@ class Solution:
             if p3:
                 p3 = p3.next
         return p1
-
-
-sol = Solution()
-l1 = List(ListNode())
-
-l1.init_from_list([1,2,3,4,5])
-print()
-print("l1:")
-l1.traverse()
-
-l2 = List(sol.reverseList(l1.root))
-print("l2:")
-l2.traverse()
-
